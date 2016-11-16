099169ca23277622079972826d4e20e61ba1f333
Rafael Moura <rafael.vrmoura@gmail.com>
2016-11-16 11:28:38 -0300

    Scenes created and pokemons added to bundle

---
+++ Stadium/TeamCell.swift
@@ -0,0 +1,24 @@
+//
+//  TeamCell.swift
+//  Stadium
+//
+//  Created by Rafael Moura on 16/11/16.
+//  Copyright © 2016 BEPiD. All rights reserved.
+//
+
+import UIKit
+
+class TeamCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
